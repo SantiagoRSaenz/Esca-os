226bf555a477cdc76e3b98acf60f79dd448a6778
Santiago Ramírez Sáenz <sramirezsa@unbosque.edu.co>
2025-02-17 20:56:51 -0500

    Inicio del proyecto de repartición de escaños

---
+++ main.py
@@ -0,0 +1,13 @@
+class reparticion_escaños:
+
+    def __init__(self, partidos, escaños, num_partidos):
+        self.partidos = partidos
+        self.escaños = escaños
+        self.num_partidos = num_partidos
+
+    def solicitud_datos(self):
+        num_partidos = input("Ingrese el número de partidos: ")
+        escaños = input("Ingrese el número de escaños: ")
+
+    def solicitud_partidos(self, num_partidos):
+        
